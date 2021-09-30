90bfb195dc9beef95350edfb45cdef9027ec84b7
owadud <akashahmed104466@gmail.com>
2021-09-30 20:06:01 +0600

    update

--- destructure.js
+++ destructure.js
@@ -1,6 +1,23 @@
 const person = { name: "jack william", age: 17, job:'facebooker', gfName:'Ema Watson', address:"Dhaka University", phone:'0154874844', friends:'kolimulla'};
 
-const {phone,gfName} = person;
+// const {phone,gfName} = person;
 
-console.log(phone,gfName);
+// console.log(phone,gfName);
 
+
+const friends = ['sakib khan','Arman khan','salman khan', 'saruk khan'];
+
+const [chotoFriend, nextfriend,...restFriends]= friends;
+console.log(chotoFriend,nextfriend,restFriends);
+
+const complexObject = {
+    name:'aksh',
+    info:{
+        address:'kola bagan',
+        leader:"tiger leader"
+    }
+}
+
+const {leader} =complexObject.info;
+
+console.log(leader);
